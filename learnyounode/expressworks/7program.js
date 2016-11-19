c887c92be14931554f780b59426b603b9ad01dd0
Etnath <kevin.giffone@gmail.com>
2016-11-19 18:47:14 +0100

    7th expressworks challenge solution

---
+++ learnyounode/expressworks/7program.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var port = process.argv[2];
+
+var app = express();
+
+app.get('/search/', function(req, res)
+{
+    var query = req.query;
+    res.send(query);
+});
+
+app.listen(port);
